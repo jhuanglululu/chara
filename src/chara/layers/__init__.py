79a9216bca35a1662f2685b35db657072b36bd93
jhuanglululu <huangchaungyun22@gmail.com>
2026-04-12 12:36:48 -0400

    feat: add rms norm wrapper

--- src/chara/layers/__init__.py
+++ src/chara/layers/__init__.py
@@ -1,4 +1,6 @@
 from .attention import Attention
 from .mlp import SwiGluMlp
+from .norm import RmsNorm
+from .block import DecoderBlock
 
-__all__ = ["Attention", "SwiGluMlp"]
+__all__ = ["Attention", "SwiGluMlp", "RmsNorm", "DecoderBlock"]
